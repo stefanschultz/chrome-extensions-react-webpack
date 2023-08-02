8073604332bf7d9a3237ceaface58d62b17ca1f3
Stefan Schultz <info@schultzsoftware.com>
2023-08-02 14:27:03 +0200

    Renamed CSS style files

--- src/pages/panel/components/Panel.tsx
+++ src/pages/panel/components/Panel.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import "../styles/Panel.styled.css";
+import "../styles/Panel.styles.css";
 
 const Panel = () => {
     return (
--- src/pages/popup/components/Popup.tsx
+++ src/pages/popup/components/Popup.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import "../styles/Popup.styled.css";
+import "../styles/Popup.styles.css";
 
 const Popup = () => {
     return (
